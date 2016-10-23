c97e011a1439d0726c3b7de5a6e53b424afb218b
smb123w64gb <smb123w64gb@gmail.com>
2016-10-22 23:59:56 -0700

    Added smb & Added DDS Nut
    "It was ez"

--- VBN Editor/Filetypes/NUT.cs
+++ VBN Editor/Filetypes/NUT.cs
@@ -33,65 +33,65 @@ namespace VBN_Editor
 
 			int[] filen = new int[count];
 
-			for(int i = 0 ; i < count ; i++){
-				padfix += headerSize;
-				d.skip(4); 
-				d.skip(4);
-				int size = d.readInt();
-				int DDSSize = size;
-				headerSize = d.readShort();
-				d.skip(5);
-				int typet = d.readByte();
+            for (int i = 0; i < count; i++) {
+                padfix += headerSize;
+                d.skip(4);
+                d.skip(4);
+                int size = d.readInt();
+                int DDSSize = size;
+                headerSize = d.readShort();
+                d.skip(5);
+                int typet = d.readByte();
 
-				int width = 0, height = 0;
-				width = d.readShort();
-				height = d.readShort();
+                int width = 0, height = 0;
+                width = d.readShort();
+                height = d.readShort();
 
-				d.skip(8);// mipmaps and padding
+                d.skip(8);// mipmaps and padding
 
-				int offset1 = d.readInt() + 16;
-				int offset2 = d.readInt() + 16;
-				//			d.skip(4);
-				int offset3 = d.readInt() + 16;
-				d.skip(4);
-				if(i == 0){
-					offheader = offset3;
-				}
+                int offset1 = d.readInt() + 16;
+                int offset2 = d.readInt() + 16;
+                //			d.skip(4);
+                int offset3 = d.readInt() + 16;
+                d.skip(4);
+                if (i == 0) {
+                    offheader = offset3;
+                }
 
-				if(headerSize == 0x90){
-					DDSSize = d.readInt();
-					d.skip(0x3C);
-				}
-				if(headerSize == 0x80){
-					DDSSize = d.readInt();
-					d.skip(44);
-				}
-				if(headerSize == 0x70){
-					DDSSize = d.readInt();
-					d.skip(28);
-				}
-				if(headerSize == 0x60){
-					DDSSize = d.readInt();
-					d.skip(12);
-				}
+                if (headerSize == 0x90) {
+                    DDSSize = d.readInt();
+                    d.skip(0x3C);
+                }
+                if (headerSize == 0x80) {
+                    DDSSize = d.readInt();
+                    d.skip(44);
+                }
+                if (headerSize == 0x70) {
+                    DDSSize = d.readInt();
+                    d.skip(28);
+                }
+                if (headerSize == 0x60) {
+                    DDSSize = d.readInt();
+                    d.skip(12);
+                }
 
-				d.skip(16);
-				d.skip(4);
-				d.skip(4);
-				int fileNum = d.readShort();
-				int subfile = d.readShort();
-				d.skip(4);
+                d.skip(16);
+                d.skip(4);
+                d.skip(4);
+                int fileNum = d.readShort();
+                int subfile = d.readShort();
+                d.skip(4);
 
-				//if(type == SMASH){
-				// TODO: Pokken type....
-				fileNum = (fileNum<<16)|subfile;
-				//}
+                //if(type == SMASH){
+                // TODO: Pokken type....
+                fileNum = (fileNum << 16) | subfile;
+                //}
 
-				Bitmap image = new Bitmap(10,10);
+                Bitmap image = new Bitmap(10, 10);
 
                 // Thanks Aelan for GTX support!
-                int t = d.pos ();
-                d.seek (offheader);
+                int t = d.pos();
+                d.seek(offheader);
                 d.skip(4);
                 int w = d.readInt();
                 int h = d.readInt();
@@ -107,24 +107,43 @@ namespace VBN_Editor
                     pitch = d.readInt();
 
                 offheader += 0x80;
-                d.seek (t);
+                d.seek(t);
 
 
                 //Console.WriteLine("Texture " + w + " " + h + " " + format + " " + tm + " " + swizzle + " " +  (offheader - 0x80));
+                if (header == 0x4E545755) {
+                byte[] data = GTX.swizzleBC(d.getSection(offset1 + padfix, DDSSize), w, h, format, tm, pitch, swizzle);
+                    switch (typet)
+                    {
+                        case 0x0:
+                            image = DDS.toBitmap(data, width, height, DDS.DDSFormat.DXT1);
+                            break;
+                        case 0x2:
+                            image = DDS.toBitmap(data, width, height, DDS.DDSFormat.DXT5);
+                            break;
+                        default:
+                            Console.WriteLine("\t" + headerSize + " Type 0x" + typet);
+                            break;
+                    }
+                }
+                if (header == 0x4E545033)
+                {
+                    byte[] data = d.getSection(offset1 + padfix, DDSSize);
+                    switch (typet)
+                    {
+                        case 0x0:
+                            image = DDS.toBitmap(data, width, height, DDS.DDSFormat.DXT1);
+                            break;
+                        case 0x2:
+                            image = DDS.toBitmap(data, width, height, DDS.DDSFormat.DXT5);
+                            break;
+                        default:
+                            Console.WriteLine("\t" + headerSize + " Type 0x" + typet);
+                            break;
+                    }
+                }
 
-                byte[] data = GTX.swizzleBC(d.getSection (offset1 + padfix, DDSSize), w, h, format, tm, pitch, swizzle);
-
-				switch (typet) {
-				case 0x0:
-					image = DDS.toBitmap (data, width, height, DDS.DDSFormat.DXT1);
-					break;
-				case 0x2:
-					image = DDS.toBitmap(data, width, height, DDS.DDSFormat.DXT5);
-					break;
-                    default:
-                        Console.WriteLine("\t" + headerSize + " Type 0x" + typet);
-                        break;
-				}
+                
 
 				// read the bitmap image
 
