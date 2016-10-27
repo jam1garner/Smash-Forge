d932fb21376a46ffa2060a5ec3931caa08dd447d
jam1garner <jam1.mcleod@hotmail.com>
2016-10-27 17:40:03 -0400

    Added mesh list and VIS0 toggles

---
+++ VBN Editor/GUI/Editors/MeshList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace VBN_Editor
+{
+    public partial class MeshList : DockContent
+    {
+        public MeshList()
+        {
+            InitializeComponent();
+            refresh();
+        }
+
+        public void refresh()
+        {
+            treeView1.Nodes.Clear();
+            foreach(ModelContainer m in Runtime.ModelContainers)
+            {
+                if (m.nud != null)
+                {
+                    foreach (NUD.Mesh mesh in m.nud.mesh)
+                    {
+                        treeView1.Nodes.Add(new TreeNode(mesh.name) { Tag = mesh, Checked = mesh.isVisible });
+                    }
+                }
+            }
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            ((NUD.Mesh)e.Node.Tag).isVisible = e.Node.Checked;
+        }
+    }
+}
