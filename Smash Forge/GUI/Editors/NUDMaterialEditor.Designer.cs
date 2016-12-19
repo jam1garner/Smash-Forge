cff78f64bb1825e7620a5953137106adfb765655
smb123w64gb <smb123w64gb@gmail.com>
2016-12-18 23:51:50 -0800

    Fixing the MatEditor
    FixItBB

--- Smash Forge/GUI/Editors/NUDMaterialEditor.Designer.cs
+++ Smash Forge/GUI/Editors/NUDMaterialEditor.Designer.cs
@@ -273,6 +273,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(121, 20);
             this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // comboBox3
             // 
