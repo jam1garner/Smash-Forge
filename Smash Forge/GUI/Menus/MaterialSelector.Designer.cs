360a8e982735388eb8137311f8598086ab409145
jam1garner <jam1.mcleod@hotmail.com>
2017-01-15 16:50:00 -0500

    Enter can now select a material (Suggested by DSX8)

--- Smash Forge/GUI/Menus/MaterialSelector.Designer.cs
+++ Smash Forge/GUI/Menus/MaterialSelector.Designer.cs
@@ -39,6 +39,7 @@
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(310, 417);
             this.treeView1.TabIndex = 0;
+            this.treeView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyDown);
             // 
             // button1
             // 
