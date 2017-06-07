ad02614b8e8206c2644bb75cce4d9cc9ba6f6100
Furil <ploaj_the_otter@yahoo.com>
2017-06-06 21:41:20 -0500

    slight rendering update

---
+++ Smash Forge/GUI/FormBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smash_Forge
+{
+    public class FormBase : Form
+    {
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            /*this.BackColor = System.Drawing.Color.Black;
+            this.ForeColor = System.Drawing.Color.Wheat;
+
+            foreach (Control c in Controls)
+            {
+                c.BackColor = System.Drawing.Color.Black;
+                c.ForeColor = System.Drawing.Color.Wheat;
+            }*/
+        }
+
+    }
+}
---
+++ Smash Forge/GUI/Nodes/BaseNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smash_Forge
+{
+    public class BaseNode : TreeNode
+    {
+        public static ContextMenuStrip _menu;
+        static BaseNode()
+        {
+            _menu = new ContextMenuStrip();
+        }
+        public BaseNode()
+        {
+            this.ContextMenuStrip = _menu;
+        }
+
+        public virtual void DoubleClicked(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+        }
+
+    }
+}
