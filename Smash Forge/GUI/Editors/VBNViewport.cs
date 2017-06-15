64ae7a8e869a6afe76d8bbcc39b77bb444a2cbc5
Struz <gbbwhittington@gmail.com>
2017-06-16 01:39:39 +1000

    Messed with hurtbox colouring, camera positioning

--- Smash Forge/GUI/Editors/VBNViewport.cs
+++ Smash Forge/GUI/Editors/VBNViewport.cs
@@ -1674,20 +1674,20 @@ namespace Smash_Forge
 
                     //va = Vector3.Transform(va, b.transform);
 
-                    GL.Color4(Color.FromArgb(80, Color.Green));
+                    GL.Color4(Color.FromArgb(80, 0x00, 0x53, 0x8A)); // Strong blue
 
                     if (Runtime.renderHurtboxesZone)
                     {
                         switch (h.Zone)
                         {
                             case Hurtbox.LW_ZONE:
-                                GL.Color4(Color.FromArgb(80, Color.Aqua));
+                                GL.Color4(Color.FromArgb(80, 0x00, 0x53, 0x8A)); // Strong blue
                                 break;
                             case Hurtbox.N_ZONE:
-                                GL.Color4(Color.FromArgb(80, Color.Green));
+                                GL.Color4(Color.FromArgb(80, 0xF6, 0x76, 0x8E)); // //Strong Purplish Pink
                                 break;
                             case Hurtbox.HI_ZONE:
-                                GL.Color4(Color.FromArgb(80, Color.Orange));
+                                GL.Color4(Color.FromArgb(80, 0xFF, 0x8E, 0x00)); //Vivid Orange Yellow
                                 break;
                         }
                     }
@@ -1942,6 +1942,15 @@ namespace Smash_Forge
             if (e.KeyChar == 'g')
             {
                 StartSavingAnimationGif();
+            }
+            if (e.KeyChar == '/')
+            {
+                width = 0;
+                height = 0;
+                lookup = 0.025f;
+                rot = 3.14159f;
+                zoom = -55;
+                Runtime.swapScaleYZPlane = true;
             }
             if (e.KeyChar == 'f')
             {
--- Smash Forge/MovesetManager.cs
+++ Smash Forge/MovesetManager.cs
@@ -212,18 +212,41 @@ namespace Smash_Forge
 
         // See https://stackoverflow.com/questions/470690/how-to-automatically-generate-n-distinct-colors
         // for a really good overview of how to use distinct colours.
+        //UIntToColor(0xFFFFB300), //Vivid Yellow
+        //UIntToColor(0xFF803E75), //Strong Purple
+        //UIntToColor(0xFFFF6800), //Vivid Orange
+        //UIntToColor(0xFFA6BDD7), //Very Light Blue
+        //UIntToColor(0xFFC10020), //Vivid Red
+        //UIntToColor(0xFFCEA262), //Grayish Yellow
+        //UIntToColor(0xFF817066), //Medium Gray
+
+        ////The following will not be good for people with defective color vision
+        //UIntToColor(0xFF007D34), //Vivid Green
+        //UIntToColor(0xFFF6768E), //Strong Purplish Pink
+        //UIntToColor(0xFF00538A), //Strong Blue
+        //UIntToColor(0xFFFF7A5C), //Strong Yellowish Pink
+        //UIntToColor(0xFF53377A), //Strong Violet
+        //UIntToColor(0xFFFF8E00), //Vivid Orange Yellow
+        //UIntToColor(0xFFB32851), //Strong Purplish Red
+        //UIntToColor(0xFFF4C800), //Vivid Greenish Yellow
+        //UIntToColor(0xFF7F180D), //Strong Reddish Brown
+        //UIntToColor(0xFF93AA00), //Vivid Yellowish Green
+        //UIntToColor(0xFF593315), //Deep Yellowish Brown
+        //UIntToColor(0xFFF13A13), //Vivid Reddish Orange
+        //UIntToColor(0xFF232C16), //Dark Olive Green
         public static readonly List<Color> distinctColors = new List<Color>()
         {
-            Color.FromArgb(0xFF, 0xCE, 0xA2, 0x62), // Grayish yellow
-            //Color.FromArgb(0xFF, 0x00, 0x7D, 0x34), // Vivid green
-            Color.FromArgb(0xFF, 0xFF, 0xB3, 0x0),  // Vivid yellow
-            Color.FromArgb(0xFF, 0xFF, 0x68, 0x00), // Vivid orange
-            Color.FromArgb(0xFF, 0xC1, 0x0, 0x20),  // Vivid red
+            //Color.FromArgb(0xFF, 0xCE, 0xA2, 0x62), // Grayish yellow
+            Color.FromArgb(0xFF, 0x00, 0x7D, 0x34),   // Vivid green
+            //Color.FromArgb(0xFF, 0xFF, 0xB3, 0x0),  // Vivid yellow
+            Color.FromArgb(0xFF, 0xF4, 0xC8, 0x00),   // Vivid Greenish Yellow
+            Color.FromArgb(0xFF, 0xFF, 0x68, 0x00),   // Vivid orange
+            Color.FromArgb(0xFF, 0xC1, 0x0, 0x20),    // Vivid red
         };
 
         public Color GetRegularDisplayColor()
         {
-            // Ranges from Red (weakest hitbox) to Yellow (strongest hitbox)
+            // Chooses different colour from distinctColours depending on knockback
             float kb = GetSimplifiedKnockback(Damage, KnockbackBase, KnockbackGrowth, 160);
             return distinctColors[getKnockbackBucket(kb)];
         }
