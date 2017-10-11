724708a40f380530928a29ec01bf9cd52b4f7fff
SMG <scandanavianmountaingoat@gmail.com>
2017-10-10 21:11:56 -0500

    MBN shader fixes

--- Smash Forge/GUI/Editors/VBNViewport.cs
+++ Smash Forge/GUI/Editors/VBNViewport.cs
@@ -2157,7 +2157,7 @@ namespace Smash_Forge
         {
             if (e.KeyChar == 'i')
             {
-                //reloadShaderFromFile("NUD", File.ReadAllText("vert.txt"), File.ReadAllText("frag.txt"));
+                //reloadShaderFromFile("MBN", File.ReadAllText("vert.txt"), File.ReadAllText("frag.txt"));
                 //reloadShaderFromFile("Texture", File.ReadAllText("vert.txt"), File.ReadAllText("frag.txt"));
             }
             /*if (e.KeyChar == 'w')
