ef0c83c03b7adb35f2a820000c8f55e422290344
SMG <scandanavianmountaingoat@gmail.com>
2017-11-17 19:58:57 -0600

    NSC shader update

--- Smash Forge/Filetypes/Models/NUD.cs
+++ Smash Forge/Filetypes/Models/NUD.cs
@@ -265,6 +265,21 @@ namespace Smash_Forge
 
             Material mat = p.materials[0];
 
+
+            //NSC
+            GL.Uniform3(shader.getAttribute("NSC"), Vector3.Zero);
+            if (p.Parent != null && p.Parent.Text.Contains("_NSC"))
+            {
+                int index = ((Mesh)p.Parent).singlebind;
+                if (index != -1)
+                    GL.Uniform3(shader.getAttribute("NSC"), Vector3.Transform(Vector3.Zero, Runtime.ModelContainers[0].vbn.bones[index].transform));
+            }
+            else
+            {
+                GL.Uniform3(shader.getAttribute("NSC"), Vector3.Zero);
+            }
+
+
             GL.Uniform1(shader.getAttribute("flags"), mat.flags);
             GL.Uniform1(shader.getAttribute("isTransparent"), p.isTransparent ? 1 : 0);
             GL.Uniform1(shader.getAttribute("selectedBoneIndex"), Runtime.selectedBoneIndex);
