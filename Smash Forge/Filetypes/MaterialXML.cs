8e3de65ea5ffd49080b4cfc8cb3ba7883197dd28
SMG <scandanavianmountaingoat@gmail.com>
2018-01-19 20:55:11 -0600

    material xml cleanup

--- Smash Forge/Filetypes/MaterialXML.cs
+++ Smash Forge/Filetypes/MaterialXML.cs
@@ -11,11 +11,9 @@ using System.Diagnostics;
 
 namespace Smash_Forge
 {
-
-
     class MaterialXML
     {
-        public class PolycountException : Exception
+        public class PolyCountException : Exception
         {
 
         }
@@ -30,7 +28,7 @@ namespace Smash_Forge
             }
         }
 
-        public static void exportMaterialAsXML(NUD n, string filename)
+        public static void ExportMaterialAsXml(NUD n, string filename)
         {
             XmlDocument doc = new XmlDocument();
 
@@ -69,59 +67,112 @@ namespace Smash_Forge
                 XmlNode matnode = doc.CreateElement("material");
                 polynode.AppendChild(matnode);
 
-                { XmlAttribute flags = doc.CreateAttribute("flags"); flags.Value = mat.Flags.ToString("x"); matnode.Attributes.Append(flags); }
-                { XmlAttribute a = doc.CreateAttribute("srcFactor"); a.Value = mat.srcFactor.ToString(); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("dstFactor"); a.Value = mat.dstFactor.ToString(); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("AlphaFunc"); a.Value = mat.AlphaFunc.ToString(); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("AlphaTest"); a.Value = mat.AlphaTest.ToString(); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("RefAlpha"); a.Value = mat.RefAlpha.ToString(); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("cullmode"); a.Value = mat.cullMode.ToString("x"); matnode.Attributes.Append(a); }
-                { XmlAttribute a = doc.CreateAttribute("zbuffoff"); a.Value = mat.zBufferOffset.ToString(); matnode.Attributes.Append(a); }
-
-                // textures
-                foreach (NUD.Mat_Texture tex in mat.textures)
-                {
-                    XmlNode texnode = doc.CreateElement("texture");
-                    { XmlAttribute a = doc.CreateAttribute("hash"); a.Value = tex.hash.ToString("x"); texnode.Attributes.Append(a); }
-                    { XmlAttribute a = doc.CreateAttribute("wrapmodeS"); a.Value = tex.WrapMode1.ToString("x"); texnode.Attributes.Append(a); }
-                    { XmlAttribute a = doc.CreateAttribute("wrapmodeT"); a.Value = tex.WrapMode2.ToString("x"); texnode.Attributes.Append(a); }
-                    { XmlAttribute a = doc.CreateAttribute("minfilter"); a.Value = tex.minFilter.ToString("x"); texnode.Attributes.Append(a); }
-                    { XmlAttribute a = doc.CreateAttribute("magfilter"); a.Value = tex.magFilter.ToString("x"); texnode.Attributes.Append(a); }
-                    { XmlAttribute a = doc.CreateAttribute("mipdetail"); a.Value = tex.mipDetail.ToString("x"); texnode.Attributes.Append(a); }
-                    matnode.AppendChild(texnode);
-                }
-
-                // params
-                foreach (KeyValuePair<string, float[]> k in mat.entries)
-                {
-                    XmlNode paramnode = doc.CreateElement("param");
-                    XmlAttribute a = doc.CreateAttribute("name"); a.Value = k.Key; paramnode.Attributes.Append(a);
-                    matnode.AppendChild(paramnode);
-
-                    if (k.Key == "NU_materialHash")
-                    {
-                        // material hash should be in hex for easier reading
-                        foreach (float f in k.Value)
-                            paramnode.InnerText += BitConverter.ToUInt32(BitConverter.GetBytes(f), 0).ToString("x") + " ";
-                    }
-                    else
-                    {
-                        int count = 0;
-                        foreach (float f in k.Value)
-                        {
-                            // only need to print 4 values and avoids tons of 0's
-                            if (count <= 4)
-                                paramnode.InnerText += f.ToString() + " ";
-                            count += 1;
-                        }
-
-                    }
-
-                }
+                WriteAttributes(doc, mat, matnode);
+                WriteTextures(doc, mat, matnode);
+                WriteMatParams(doc, mat, matnode);
             }
         }
 
-        public static void importMaterialAsXML(NUD n, string filename)
+        private static void WriteAttributes(XmlDocument doc, NUD.Material mat, XmlNode matnode)
+        {
+            { XmlAttribute flags = doc.CreateAttribute("flags"); flags.Value = mat.Flags.ToString("x"); matnode.Attributes.Append(flags); }
+            { XmlAttribute a = doc.CreateAttribute("srcFactor"); a.Value = mat.srcFactor.ToString(); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("dstFactor"); a.Value = mat.dstFactor.ToString(); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("AlphaFunc"); a.Value = mat.AlphaFunc.ToString(); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("AlphaTest"); a.Value = mat.AlphaTest.ToString(); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("RefAlpha"); a.Value = mat.RefAlpha.ToString(); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("cullmode"); a.Value = mat.cullMode.ToString("x"); matnode.Attributes.Append(a); }
+            { XmlAttribute a = doc.CreateAttribute("zbuffoff"); a.Value = mat.zBufferOffset.ToString(); matnode.Attributes.Append(a); }
+        }
+
+        private static void WriteMatParams(XmlDocument doc, NUD.Material mat, XmlNode matnode)
+        {
+            foreach (KeyValuePair<string, float[]> k in mat.entries)
+            {
+                XmlNode paramnode = doc.CreateElement("param");
+                XmlAttribute a = doc.CreateAttribute("name"); a.Value = k.Key; paramnode.Attributes.Append(a);
+                matnode.AppendChild(paramnode);
+
+                if (k.Key == "NU_materialHash")
+                {
+                    // material hash should be in hex for easier reading
+                    foreach (float f in k.Value)
+                        paramnode.InnerText += BitConverter.ToUInt32(BitConverter.GetBytes(f), 0).ToString("x") + " ";
+                }
+                else
+                {
+                    int count = 0;
+                    foreach (float f in k.Value)
+                    {
+                        // only need to print 4 values and avoids tons of 0's
+                        if (count <= 4)
+                            paramnode.InnerText += f.ToString() + " ";
+                        count += 1;
+                    }
+
+                }
+
+            }
+        }
+
+        private static void WriteTextures(XmlDocument doc, NUD.Material mat, XmlNode matnode)
+        {
+            foreach (NUD.Mat_Texture tex in mat.textures)
+            {
+                XmlNode texnode = doc.CreateElement("texture");
+                { XmlAttribute a = doc.CreateAttribute("hash"); a.Value = tex.hash.ToString("x"); texnode.Attributes.Append(a); }
+                { XmlAttribute a = doc.CreateAttribute("wrapmodeS"); a.Value = tex.WrapMode1.ToString("x"); texnode.Attributes.Append(a); }
+                { XmlAttribute a = doc.CreateAttribute("wrapmodeT"); a.Value = tex.WrapMode2.ToString("x"); texnode.Attributes.Append(a); }
+                { XmlAttribute a = doc.CreateAttribute("minfilter"); a.Value = tex.minFilter.ToString("x"); texnode.Attributes.Append(a); }
+                { XmlAttribute a = doc.CreateAttribute("magfilter"); a.Value = tex.magFilter.ToString("x"); texnode.Attributes.Append(a); }
+                { XmlAttribute a = doc.CreateAttribute("mipdetail"); a.Value = tex.mipDetail.ToString("x"); texnode.Attributes.Append(a); }
+                matnode.AppendChild(texnode);
+            }
+        }
+
+        public static void ImportMaterialAsXml(NUD n, string filename)
+        {
+            // Creates a list of materials and then trys to apply the materials to the polygons. 
+
+            int polyCount = CalculatePolygonCount(n);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename);
+
+            XmlNode main = doc.ChildNodes[0];
+
+            List<NUD.Material> materialList = new List<NUD.Material>();
+            List<int> polygonMatCount = new List<int>();
+
+            // Validate at every step
+            foreach (XmlNode meshNode in main.ChildNodes)
+            {
+                if (meshNode.Name.Equals("mesh"))
+                {
+                    foreach (XmlNode polynode in meshNode.ChildNodes)
+                    {
+                        if (polynode.Name.Equals("polygon"))
+                        {
+                            polygonMatCount.Add(polynode.ChildNodes.Count);
+
+                            if (polygonMatCount.Count > polyCount)
+                            {
+                                int countDif = polygonMatCount.Count - polyCount;
+                                MessageBox.Show(String.Format("Expected {0} polygons but found {1} in the XML file. " +
+                                    "The last {2} polygon(s) will be ignored.",
+                                    polyCount, polygonMatCount.Count, countDif));
+                            }
+
+                            ReadMaterials(materialList, polynode);
+                        }
+                    }
+                }
+            }
+
+            ApplyMaterials(n, materialList, polygonMatCount);
+        }
+
+        private static int CalculatePolygonCount(NUD n)
         {
             int polyCount = 0;
             foreach (NUD.Mesh m in n.Nodes)
@@ -132,84 +183,65 @@ namespace Smash_Forge
                 }
             }
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load(filename);
+            return polyCount;
+        }
 
-            XmlNode main = doc.ChildNodes[0];
-
-            List<NUD.Material> materials = new List<NUD.Material>();
-            List<int> ids = new List<int>();
-
-            // validate at every step
-            foreach (XmlNode meshnode in main.ChildNodes)
-            {
-                if (meshnode.Name.Equals("mesh"))
-                {
-                    foreach (XmlNode polynode in meshnode.ChildNodes)
-                    {
-                        if (polynode.Name.Equals("polygon"))
-                        {
-                            ids.Add(polynode.ChildNodes.Count);
-
-                            if (ids.Count > polyCount)
-                            {
-                                int countDif = ids.Count - polyCount;
-                                MessageBox.Show(String.Format("Expected {0} polygons but found {1} in the XML file. " +
-                                    "The last {2} polygon(s) will be ignored.",
-                                    polyCount, ids.Count, countDif));
-                            }
-
-                            ReadMaterials(materials, polynode);
-                        }
-                    }
-                }
-            }
-
-            int pid = 0;
-            int mid = 0;
+        private static void ApplyMaterials(NUD n, List<NUD.Material> materialList, List<int> polyMatCount)
+        {
+            int matIndex = 0;
+            int polyIndex = 0;
             foreach (NUD.Mesh m in n.Nodes)
             {
                 foreach (NUD.Polygon p in m.Nodes)
                 {
                     p.materials.Clear();
-                    for (int i = 0; i < ids[pid]; i++)
+                    for (int i = 0; i < polyMatCount[polyIndex]; i++)
                     {
-                        p.materials.Add(materials[mid++]);
+                        p.materials.Add(materialList[matIndex]);
+                        matIndex += 1;
                     }
-                    pid++;
+                
+                    polyIndex += 1;
                 }
             }
         }
 
-        private static void ReadMaterials(List<NUD.Material> matList, XmlNode polyNode)
+        private static void ReadMaterials(List<NUD.Material> materialList, XmlNode polyNode)
         {
             foreach (XmlNode matnode in polyNode.ChildNodes)
             {
                 if (matnode.Name.Equals("material"))
                 {
                     NUD.Material mat = new NUD.Material();
-                    matList.Add(mat);
-                    foreach (XmlAttribute a in matnode.Attributes)
-                    {
-                        switch (a.Name)
-                        {
-                            case "flags": uint f = 0; if (uint.TryParse(a.Value, NumberStyles.HexNumber, null, out f)) { mat.Flags = f; }; break;
-                            case "srcFactor": int.TryParse(a.Value, out mat.srcFactor); break;
-                            case "dstFactor": int.TryParse(a.Value, out mat.dstFactor); break;
-                            case "AlphaFunc": int.TryParse(a.Value, out mat.AlphaFunc); break;
-                            case "AlphaTest": int.TryParse(a.Value, out mat.AlphaTest); break;
-                            case "RefAlpha": int.TryParse(a.Value, out mat.RefAlpha); break;
-                            case "cullmode": int cm = 0; if (int.TryParse(a.Value, NumberStyles.HexNumber, null, out cm)) { mat.cullMode = cm; }; break;
-                            case "zbuffoff": int.TryParse(a.Value, out mat.zBufferOffset); break;
-                        }
-                    }
+                    materialList.Add(mat);
+
+                    ReadAttributes(matnode, mat);
 
                     foreach (XmlNode mnode in matnode.ChildNodes)
                     {
-                        ReadTextures(mat, mnode);
-                        ReadMatParams(polyNode, mat, mnode);
-
+                        if (mnode.Name.Equals("texture"))
+                            ReadTextures(mat, mnode);
+                        else if (mnode.Name.Equals("param"))
+                            ReadMatParams(polyNode, mat, mnode);
                     }
+                }
+            }
+        }
+
+        private static void ReadAttributes(XmlNode matnode, NUD.Material mat)
+        {
+            foreach (XmlAttribute a in matnode.Attributes)
+            {
+                switch (a.Name)
+                {
+                    case "flags": uint f = 0; if (uint.TryParse(a.Value, NumberStyles.HexNumber, null, out f)) { mat.Flags = f; }; break;
+                    case "srcFactor": int.TryParse(a.Value, out mat.srcFactor); break;
+                    case "dstFactor": int.TryParse(a.Value, out mat.dstFactor); break;
+                    case "AlphaFunc": int.TryParse(a.Value, out mat.AlphaFunc); break;
+                    case "AlphaTest": int.TryParse(a.Value, out mat.AlphaTest); break;
+                    case "RefAlpha": int.TryParse(a.Value, out mat.RefAlpha); break;
+                    case "cullmode": int cm = 0; if (int.TryParse(a.Value, NumberStyles.HexNumber, null, out cm)) { mat.cullMode = cm; }; break;
+                    case "zbuffoff": int.TryParse(a.Value, out mat.zBufferOffset); break;
                 }
             }
         }
@@ -220,6 +252,7 @@ namespace Smash_Forge
             {
                 NUD.Mat_Texture tex = new NUD.Mat_Texture();
                 mat.textures.Add(tex);
+
                 foreach (XmlAttribute a in matNode.Attributes)
                 {
                     switch (a.Name)
@@ -237,66 +270,68 @@ namespace Smash_Forge
 
         private static void ReadMatParams(XmlNode polyNode, NUD.Material mat, XmlNode matNode)
         {
-            if (matNode.Name.Equals("param"))
+            if (!matNode.Name.Equals("param"))
+                return;
+
+            string name = ReadName(matNode);
+            List<float> valueList = ReadParamValues(matNode, name);
+
+            // Parameters should always have 4 values.                                           
+            if (valueList.Count != 4)
+                throw new ParamArrayLengthException(polyNode.ChildNodes.Count, name);
+
+            // Using a dictionary prevents duplicate material parameters.
+            try
             {
-                string name = ReadMatParamName(matNode);
-
-                string[] values = matNode.InnerText.Split(' ');
-                List<float> v = new List<float>();
-                float f = 0;
-
-                foreach (string stringValue in values)
-                {
-                    if (v.Count < 4)
-                    {
-                        if (name == "NU_materialHash")
-                        {
-                            int hash;
-                            if (int.TryParse(stringValue, NumberStyles.HexNumber, null, out hash))
-                            {
-                                f = BitConverter.ToSingle(BitConverter.GetBytes(hash), 0);
-                                v.Add(f);
-                            }
-                        }
-                        else if (float.TryParse(stringValue, out f))
-                            v.Add(f);
-                        else
-                            v.Add(0.0f);
-                    }
-                }
-
-                // array should always have 4 values                                           
-                if (v.Count != 4)
-                {
-                    throw new ParamArrayLengthException(polyNode.ChildNodes.Count, name);
-                }
-
-                try
-                {
-                    mat.entries.Add(name, v.ToArray());
-                }
-                catch (System.ArgumentException)
-                {
-                    MessageBox.Show(String.Format("Polygon{0} contains more than 1 instance of {1}. \n"
-                        + "Only the first instance of {1} will be added.", polyNode.ChildNodes.Count.ToString(), name));
-                }
+                mat.entries.Add(name, valueList.ToArray());
             }
+            catch (System.ArgumentException)
+            {
+                MessageBox.Show(String.Format("Polygon{0} contains more than 1 instance of {1}. \n"
+                    + "Only the first instance of {1} will be added.", polyNode.ChildNodes.Count.ToString(), name));
+            }        
         }
 
-        private static string ReadMatParamName(XmlNode matNode)
+        private static List<float> ReadParamValues(XmlNode matNode, string name)
         {
-            string name = "";
+            string[] values = matNode.InnerText.Split(' ');
+            List<float> valueList = new List<float>();
+            foreach (string stringValue in values)
+            {
+                if (valueList.Count >= 4)
+                    break;
 
-            foreach (XmlAttribute a in matNode.Attributes)
+                float f = 0;
+
+                if (name == "NU_materialHash")
+                {
+                    int hash;
+                    if (int.TryParse(stringValue, NumberStyles.HexNumber, null, out hash))
+                    {
+                        f = BitConverter.ToSingle(BitConverter.GetBytes(hash), 0);
+                        valueList.Add(f);
+                    }
+                }
+                else if (float.TryParse(stringValue, out f))
+                    valueList.Add(f);
+                else
+                    valueList.Add(0.0f);            
+            }
+
+            return valueList;
+        }
+
+        private static string ReadName(XmlNode node)
+        {
+            foreach (XmlAttribute a in node.Attributes)
             {
                 if (a.Name == "name")
                 {
-                    name = a.Value;
-                    break;
+                    return a.Value;
                 }
             }
 
-            return name;
+            return "";
         }
     }
 }
