126d18cd4207f558c4cc9f3b9a6f4d51ffd7dae7
SMG <scandanavianmountaingoat@gmail.com>
2018-02-25 14:10:46 -0600

    remove black box from mainform

--- Smash Forge/GUI/MainForm.cs
+++ Smash Forge/GUI/MainForm.cs
@@ -2303,8 +2303,10 @@ namespace Smash_Forge
 
         private void glControl1_Paint(object sender, PaintEventArgs e)
         {
+            // I got tired of looking at it.
             glControl1.MakeCurrent();
-            Rendering.RenderTools.RenderBackground();
+            GL.ClearColor(System.Drawing.Color.FromArgb(255, 250, 250, 250));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
             glControl1.SwapBuffers();
         }
 
