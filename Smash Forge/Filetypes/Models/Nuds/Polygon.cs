71f35ae4310e89f9b005ed73dd270b12386413bd
SMG <scandanavianmountaingoat@gmail.com>
2018-08-20 14:10:19 -0500

    simplify nud render mesh creation

--- Smash Forge/Filetypes/Models/Nuds/Polygon.cs
+++ Smash Forge/Filetypes/Models/Nuds/Polygon.cs
@@ -98,20 +98,8 @@ namespace Smash_Forge
 
             public void GetDisplayVerticesAndIndices(out List<DisplayVertex> displayVerticesList, out List<int> vertexIndicesList)
             {
-                int vertexOffset = 0;
-
-                displayVerticesList = new List<DisplayVertex>();
-                vertexIndicesList = new List<int>();
-
-                List<DisplayVertex> polygonDisplayVertices = CreateDisplayVertices();
-                displayVerticesList.AddRange(polygonDisplayVertices);
-
-                for (int i = 0; i < displayFaceSize; i++)
-                {
-                    vertexIndicesList.Add(display[i] + vertexOffset);
-                }
-
-                vertexOffset += polygonDisplayVertices.Count;
+                displayVerticesList = CreateDisplayVertices();
+                vertexIndicesList = new List<int>(display);
             }
 
             private List<DisplayVertex> CreateDisplayVertices()
